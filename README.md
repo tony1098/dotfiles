c93ef30240ba316f92f6992556a265b91d5dc9f1
Tony Tarng <tonytarng1098@gmail.com>
2020-10-17 02:06:25 +0800

    Initial commit: Add zsh and vim config

---
+++ README.md
@@ -0,0 +1,6 @@
+## My Dotfiles
+
+Custom configuration files for:
+* Zsh
+	* Requires zsh 4.3.11+ for syntax highlighting and autosuggestions
+* Vim
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env zsh
+
+#################
+# ZSH
+#################
+
+echo "Starting configuration"
+
+# Fast syntax highlighting
+if [[ ! -d ~/.zsh/zsh-syntax-highlighting ]]; then
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/dotfiles/zsh/zsh-syntax-highlighting
+fi
+
+# Autosuggestions
+if [[ ! -d ~/.zsh/zsh-autosuggestions ]]; then
+	git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/dotfiles/zsh/zsh-autosuggestions
+fi
+
+#################
+# VIM
+#################
+if [[ ! -d ~/.vim/vim-code-dark ]]; then
+	git clone https://github.com/tomasiser/vim-code-dark.git ~/dotfiles/vim/vim-code-dark
+fi
+
+ln -sv ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sv ~/dotfiles/vim/.vimrc ~/.vimrc
+
+echo "Done!"
